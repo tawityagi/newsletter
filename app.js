7cf85b2683b348c9e49eed218d970ac363db1696
Tawi Tyagi <79146317+tawityagi@users.noreply.github.com>
2021-07-13 23:57:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+require("dotenv").config()
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/",function (req,res) {
+    res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/",function (req,res) {
+    const firstName=req.body.fName;
+    const lastName=req.body.lName;
+    const email=req.body.email;
+    console.log(firstName,lastName,email);
+
+    const data= {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields : {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData= JSON.stringify(data);
+    const url=process.env.URL;
+    const options= {
+        method: "POST",
+        auth: process.env.AUTH
+    }
+    const reques= https.request(url,options,function (response) {
+        if(response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html");
+        } else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data",function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    reques.write(jsonData);
+    reques.end();
+})
+
+app.post("/failure",function (req,res) {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function () {
+    console.log("Server is running....");
+})
+
